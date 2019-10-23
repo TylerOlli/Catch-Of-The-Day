e69086d0bb882a7f9454b6154c563cc96ebe40ea
TylerOlli <ollityler@gmail.com>
2019-10-22 22:23:44 -0400

    Setup Event Handling for StorePicker Submit Button

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,12 +2,24 @@ import React from "react";
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = event => {
+    // 1. Stop Form From Submitting
+    event.preventDefault();
+    // 2. Get Text From Input
+    const storeName = this.myInput.current.value;
+    // 3. Change The Page to /store/...
+    this.props.history.push(`/store/${storeName}`);
+  };
+
   render() {
     return (
-      <form action='' className='store-selector'>
+      <form action='' className='store-selector' onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
         <input
           type='text'
+          ref={this.myInput}
           required
           placeholder='Store Name'
           defaultValue={getFunName()}
