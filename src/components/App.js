73c6272c2066b461cb25dd2962932cda6e768f04
TylerOlli <ollityler@gmail.com>
2019-10-26 18:32:32 -0400

    Persisted State with localstorage

--- src/components/App.js
+++ src/components/App.js
@@ -13,10 +13,23 @@ class App extends React.Component {
   };
   componentDidMount() {
     const { params } = this.props.match;
+    // First Reinstate Local Storage
+    const localStorageRef = localStorage.getItem(params.storeId);
+    if (localStorageRef) {
+      this.setState({ order: JSON.parse(localStorageRef) });
+    }
     this.ref = base.syncState(`${params.storeId}/fishes`, {
       context: this,
       state: "fishes"
     });
+  }
+
+  componentDidUpdate() {
+    console.log(this.state.order);
+    localStorage.setItem(
+      this.props.match.params.storeId,
+      JSON.stringify(this.state.order)
+    );
   }
 
   componentWillUnmount() {
